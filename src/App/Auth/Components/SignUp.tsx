afc9dd84393da7a996b224d86273336e01cfe13f
Kishida masataka <fstt.fwmym.f15@gmail.com>
2017-09-11 22:02:09 +0900

    Implemented SignUp component to call API

--- src/App/Auth/Components/SignUp.tsx
+++ src/App/Auth/Components/SignUp.tsx
@@ -1,23 +1,55 @@
 import * as React from 'react';
 import { ReduxAction } from '../../../Redux/ConfigureStores';
 import { Dispatch } from 'redux';
+import { createSignUpRequestAction } from '../ActionCreators';
 
 interface Props {
   dispatch: Dispatch<ReduxAction>;
 }
 
-export class SignUp extends React.Component<Props, {}> {
+interface ComponentState {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export class SignUp extends React.Component<Props, ComponentState> {
+  constructor(props: Props) {
+    super(props);
+    this.initComponentState();
+
+    this.handleOnChange = this.handleOnChange.bind(this);
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
+
   render() {
     return (
       <div>
         <h2>SignUp</h2>
         <form>
-          <label>Name: <input placeholder="User name"/></label>
-          <label>Email: <input type="email"/></label>
-          <label>Password: <input type="password"/></label>
-          <input type="button" value="SignUp"/>
+          <label htmlFor="name">Name: <input id="name" name="name" value={this.state.name} onChange={this.handleOnChange} placeholder="User name"/></label>
+          <label>Email: <input type="email" id="email" name="email" value={this.state.email} onChange={this.handleOnChange}/></label>
+          <label>Password: <input type="password" id="password" name="password" value={this.state.password} onChange={this.handleOnChange}/></label>
+          <input type="button" value="SignUp" onClick={this.handleOnClick}/>
         </form>
       </div>
     );
+  }
+
+  private initComponentState() {
+    this.state = {
+      name: '',
+      email: '',
+      password: ''
+    };
+  }
+
+  handleOnChange(e: React.FormEvent<HTMLInputElement>) {
+    this.setState({ ...this.state, [e.currentTarget.name]: e.currentTarget.value });
+  }
+
+  handleOnClick() {
+    this.props.dispatch(createSignUpRequestAction(this.state.name, this.state.email, this.state.password));
+    this.initComponentState();
   }
 }
